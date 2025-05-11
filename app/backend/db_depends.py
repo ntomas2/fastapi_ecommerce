9279f4c0f09579e68f7a957359349e0ff392669f
Kornilov Dmitry <kornilov.da.98@gmail.com>
2025-05-11 16:51:49 +0300

    Assigned db session to variable

--- app/backend/db_depends.py
+++ app/backend/db_depends.py
@@ -1,5 +1,6 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
 
 from app.backend.db import async_session_maker
 
@@ -7,3 +8,6 @@ from app.backend.db import async_session_maker
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+DBSessionDep = Annotated[AsyncSession, Depends(get_db)]
